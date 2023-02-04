2324c6bdd0c39a04851db06f44ee8b614537090d
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-02-04 07:54:12 -0500

    Add elevation input and visualization

--- project/Potato/Potato.gd
+++ project/Potato/Potato.gd
@@ -2,6 +2,11 @@ extends KinematicBody2D
 
 signal fired(bullet)
 
+## Radians per second
+const WEAPON_ROTATION_SPEED := TAU/8.0
+
+enum _Facing { LEFT, RIGHT }
+
 ## Index of the controlling player (0-based)
 export var player_index := 0
 
@@ -12,23 +17,37 @@ var _GRAVITY : float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _velocity := Vector2.ZERO
 var _shoot_press_time := 0.0
 var _can_shoot := true
+var _facing = _Facing.RIGHT
+var _elevation := 0.0
+
+onready var _weapon_hinge := $WeaponHinge
 
 
 func _physics_process(delta:float)->void:
 	if not active:
 		return
 	
-	$WeaponHinge.rotation = get_angle_to(get_global_mouse_position())
-	
 	var action_prefix := "p%d_" % player_index
 	var direction := Input.get_axis(action_prefix + "left", action_prefix + "right")
 	var linear_velocity := Vector2(200*direction, 0)
+
+	if direction!=0:
+		_facing = _Facing.RIGHT if direction>0 else _Facing.LEFT
+		# Setting scale.x to -1 or 1 does not work because then the child's
+		# rotation gets unpredictable. Instead, later, the rotation is updated
+		# based on facing. Here, then, we have to flip the hinge's y scale so
+		# that it's not upside-down.
+		_weapon_hinge.scale.y = -1 if direction<0 else 1
 	
 	# Only pull downward when in the air
 	if not is_on_floor():
 		linear_velocity.y = _GRAVITY
 	
 	_velocity = move_and_slide(linear_velocity, Vector2.UP)
+	
+	var elevation_input := Input.get_axis(action_prefix + "up", action_prefix + "down")
+	_elevation = clamp(_elevation + elevation_input * WEAPON_ROTATION_SPEED * delta, -TAU/4.0, 0)
+	_weapon_hinge.rotation = _elevation if _facing==_Facing.RIGHT else PI - _elevation
 	
 	if Input.is_action_pressed("p0_shoot") and _shoot_press_time < 1.0 and _can_shoot:
 		_shoot_press_time += delta

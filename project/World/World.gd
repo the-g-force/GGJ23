e90fc3bfdefcb2eed847206a18aefcf14bcc1a76
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-02-03 19:10:01 -0500

    Rename variables and add comments for clarity

--- project/World/World.gd
+++ project/World/World.gd
@@ -1,26 +1,24 @@
 extends Node2D
 
-onready var _collision_polygon : CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
+onready var _ground_polygon : CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
 
 func _input(event):
 	if event.is_pressed():
-		print("ping")
 		clip($Polygon2D)
 
 
-func _process(delta):
-	update()
-
-
 func clip(polygon:Polygon2D)->void:
-	var offset_polygon : PoolVector2Array = []
+	var offset_polygon := []
 	
 	for point in polygon.polygon:
 		offset_polygon.append(point + polygon.global_position)
-	var new_ground_polygon : PoolVector2Array = Geometry.clip_polygons_2d(_collision_polygon.polygon, offset_polygon)[0]
-	_collision_polygon.set_deferred("polygon", new_ground_polygon)
+	var clip_result := Geometry.clip_polygons_2d(_ground_polygon.polygon, offset_polygon)
+	
+	# clip_polygons_2d returns an array of polygon results. Right now,
+	# we are only using the first one.
+	_ground_polygon.set_deferred("polygon", clip_result[0])
 	update()
 
 
 func _draw()->void:
-	draw_colored_polygon(_collision_polygon.polygon, Color.blue)
+	draw_colored_polygon(_ground_polygon.polygon, Color.blue)

57db766520b836e64a990879ce17d3383f2905ef
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-02-03 19:16:19 -0500

    Add terrain-destroying bullet

--- project/Bullet/Bullet.gd
+++ project/Bullet/Bullet.gd
@@ -1,21 +1,17 @@
 extends RigidBody2D
 
-export var explosion_radius := 0.0
-
-var _explosion_shape := []
+onready var _explosion_shape := $ExplosionShape
 
 
 func _ready()->void:
-	for i in 16:
-		_explosion_shape.append(Vector2.RIGHT.rotated(TAU * i / 16) * explosion_radius)
+	_explosion_shape.visible = false
+
+
+func _on_Bullet_body_shape_entered(body_rid, body, body_shape_index, local_shape_index)->void:
+	if body.get_parent().has_method("clip"):
+		body.get_parent().clip(_explosion_shape)
+		queue_free()
 
 
 func _draw()->void:
 	draw_circle(Vector2.ZERO, 10, Color.red)
-
-
-func _on_Bullet_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	pass
-#	if body.get_parent().has_method("clip"):
-#		body.get_parent().clip(_explosion_shape, global_position)
-#		queue_free()

5feb87a432772333721b65d321aa069114ec16ef
Paul Gestwicki <pvgestwicki@bsu.edu>
2023-02-04 11:51:08 -0500

    Fix turn alternation and non-turn gravity application

--- project/Potato/Potato.gd
+++ project/Potato/Potato.gd
@@ -2,6 +2,9 @@ extends KinematicBody2D
 
 signal fired(bullet)
 
+## Walking speed
+const SPEED := 200.0
+
 ## Radians per second
 const WEAPON_ROTATION_SPEED := TAU/8.0
 
@@ -18,7 +21,7 @@ export var player_index := 0
 
 var active := false setget _set_active
 
-var _GRAVITY : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _GRAVITY : float = 10 #ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var _velocity := Vector2.ZERO
 var _facing = _Facing.RIGHT
@@ -31,41 +34,49 @@ var _hold_duration := 0.0
 onready var _weapon_hinge := $WeaponHinge
 onready var _arrow : Sprite = $"%Arrow"
 onready var _health_bar : ProgressBar = $HealthBar
+onready var _action_prefix := "p%d_" % player_index
+
+
+func _ready():
+	_set_active(false)
 
 
 func _physics_process(delta:float)->void:
 	_health_bar.value = _health
 	
-	var action_prefix := "p%d_" % player_index
-	var direction := Input.get_axis(action_prefix + "left", action_prefix + "right")
-	var linear_velocity := Vector2(200*direction, 0)
-
-	if active and direction!=0:
-		_facing = _Facing.RIGHT if direction>0 else _Facing.LEFT
-		# Setting scale.x to -1 or 1 does not work because then the child's
-		# rotation gets unpredictable. Instead, later, the rotation is updated
-		# based on facing. Here, then, we have to flip the hinge's y scale so
-		# that it's not upside-down.
-		_weapon_hinge.scale.y = -1 if direction<0 else 1
-	
-	# Only pull downward when in the air
-	if not is_on_floor():
-		linear_velocity.y = _GRAVITY
-	
-	_velocity = move_and_slide(linear_velocity, Vector2.UP)
-	
+	_velocity.y += _GRAVITY
+		
 	if active:
-		var elevation_input := Input.get_axis(action_prefix + "up", action_prefix + "down")
+		var direction := Input.get_axis(_action_prefix + "left", _action_prefix + "right")
+		if direction!=0:
+			_facing = _Facing.RIGHT if direction>0 else _Facing.LEFT
+			_velocity.x = SPEED if direction > 0 else -SPEED
+			
+			# Setting scale.x to -1 or 1 does not work because then the child's
+			# rotation gets unpredictable. Instead, later, the rotation is updated
+			# based on facing. Here, then, we have to flip the hinge's y scale so
+			# that it's not upside-down.
+			_weapon_hinge.scale.y = -1 if direction<0 else 1
+		else:
+			_velocity.x = 0
+			
+		var elevation_input := Input.get_axis(_action_prefix + "up", _action_prefix + "down")
 		_elevation = clamp(_elevation + elevation_input * WEAPON_ROTATION_SPEED * delta, -TAU/4.0, 0)
 		_weapon_hinge.rotation = _elevation if _facing==_Facing.RIGHT else PI - _elevation
 		
-		var percent := clamp(range_lerp(_hold_duration, 0, MAX_HOLD_DURATION, 0, 1), 0, 1)
-		if Input.is_action_pressed(action_prefix + "shoot"):
-			_hold_duration += delta
-			_set_shader_percent(percent)
-		elif Input.is_action_just_released(action_prefix + "shoot"):
-			_shoot(percent)
-			_hold_duration = 0.0
+		_process_shooting(delta)
+	
+	_velocity = move_and_slide_with_snap(_velocity, Vector2(0,40), Vector2.UP, true)
+
+
+func _process_shooting(delta:float)->void:
+	var percent := clamp(range_lerp(_hold_duration, 0, MAX_HOLD_DURATION, 0, 1), 0, 1)
+	if Input.is_action_pressed(_action_prefix + "shoot"):
+		_hold_duration += delta
+		_set_shader_percent(percent)
+	elif Input.is_action_just_released(_action_prefix + "shoot"):
+		_shoot(percent)
+		_hold_duration = 0.0
 			
 
 ## Shoot a projectile
